37df764885662e9ace544897f550e03f4ea0236e
Clarathms <clarathaisms12@gmail.com>
2023-02-24 16:32:40 -0300

    View das Playlists + Correção Tutorial

--- Songo/Scene/Tutorial/View/TutorialBase.swift
+++ Songo/Scene/Tutorial/View/TutorialBase.swift
@@ -16,14 +16,15 @@ struct TutorialBase: View {
     // tutorial
     @State private var selectedTab = 1
     @State private var lastTab = 0
-   // @Binding var showingTutorial:Bool
+    
+    @State var chamaBotao : Bool = false
     @State private var isShowingOffer = true
     @State private var subscriptionOfferOptions: MusicSubscriptionOffer.Options = .default
-    //let appleMusicController: AppleMusicController = AppleMusicController()
+  //  let appleMusicController: AppleMusicController = AppleMusicController()
     
 //    @State var chamaBotao : Bool = false
-//    //@State var isShowingOffer: Bool = true
-//    let appleMusicService: AppleMusicService = AppleMusicService()
+    //@State var isShowingOffer: Bool = true
+    let appleMusicService: AppleMusicService = AppleMusicService()
 
 
     var body: some View {
@@ -33,36 +34,36 @@ struct TutorialBase: View {
                 VStack(alignment: .leading, spacing: 0){
                     TabView(selection: $selectedTab){
                         
-                        TutorialPage1()
+                        TutorialPage1(chamaBotao: $chamaBotao)
                             .tabItem({
                                 Image(systemName: "circle")
                             })
                             .tag(1)
-//                            .task {
-//                                let subCheck = await appleMusicService.lastSubscriptionUpdate().makeSubscriptionOffer
-//                                DispatchQueue.main.async {
-//                                    subscriptionOfferOptions.messageIdentifier = .playMusic
-//                                    isShowingOffer = subCheck
-//                                    if isShowingOffer != subCheck{
-//                                        isShowingOffer = false
-//                                    }
-//                                    if !isShowingOffer{
-//                                        chamaBotao = true
-//                                    }
-//                                }
-//                            }
-//                            .musicSubscriptionOffer(isPresented: $isShowingOffer, options: subscriptionOfferOptions)
-//                                .onAppear{
-//                                    appleMusicService.checkAppleMusicAuthorization()
-//                                    isShowingOffer = true
-//                                    
-//                                }
+                            .task {
+                                let subCheck = await appleMusicService.lastSubscriptionUpdate().makeSubscriptionOffer
+                                DispatchQueue.main.async {
+                                    subscriptionOfferOptions.messageIdentifier = .playMusic
+                                    isShowingOffer = subCheck
+                                    if isShowingOffer != subCheck{
+                                        isShowingOffer = false
+                                    }
+                                    if !isShowingOffer{
+                                        chamaBotao = true
+                                    }
+                                }
+                            }
+                            .musicSubscriptionOffer(isPresented: $isShowingOffer, options: subscriptionOfferOptions)
+                                .onAppear{
+                                    appleMusicService.checkAppleMusicAuthorization()
+                                    isShowingOffer = true
+                                    
+                                }
                         
-                        TutorialPage2().tabItem({
+                        TutorialPage2(chamaBotao: $chamaBotao).tabItem({
                             Image(systemName: "circle")
                         }).tag(2)
                         
-                        TutorialPage3().tabItem({
+                        TutorialPage3(chamaBotao: $chamaBotao).tabItem({
                             Image(systemName: "circle")
                         }).tag(3)
                        
@@ -114,9 +115,9 @@ extension TutorialBase {
 
 }
 
-
-struct AuthorizationView_Previews: PreviewProvider {
-    static var previews: some View {
-        TutorialBase()
-    }
-}
+//
+//struct AuthorizationView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        TutorialBase()
+//    }
+//}

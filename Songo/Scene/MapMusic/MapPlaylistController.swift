8d25275052f81953113dd735767991ad09d5e80d
Clarathms <clarathaisms12@gmail.com>
2023-03-03 17:36:29 -0300

    sla

--- Songo/Scene/MapMusic/MapPlaylistController.swift
+++ Songo/Scene/MapMusic/MapPlaylistController.swift
@@ -41,6 +41,8 @@ class MapPlaylistController: BaseViewController<MapPlaylistView> {
     var albumPicture: UIImage?
     
     var primeiraMusica: String?
+    var titleString: String?
+
     
 //    var toCoverView: UIImageView = {
 //        toCoverView.layer.cornerRadius = 10
@@ -113,6 +115,7 @@ class MapPlaylistController: BaseViewController<MapPlaylistView> {
 
 
         addImgCapa()
+     //   addImageLabels()
        // view.addSubview(self.toCoverView!)
         view.addSubview(tableView)
         setTableViewConstrains()
@@ -149,6 +152,23 @@ class MapPlaylistController: BaseViewController<MapPlaylistView> {
 
     }
     
+    func addImageLabels() {
+    
+        var titleLabel: UILabel?
+     //   self.primeiraMusica = titleString[0]
+        
+        titleLabel?.text = self.primeiraMusica
+        titleLabel?.numberOfLines = 0
+        titleLabel?.adjustsFontSizeToFitWidth = true
+        titleLabel?.textColor = .white
+        titleLabel?.font = .systemFont(ofSize: 16, weight: .bold)
+        titleLabel?.frame = CGRect(x: UIScreen.main.bounds.midX*0.45, y: UIScreen.main.bounds.midY, width: UIScreen.main.bounds.width/1.8, height: UIScreen.main.bounds.width/1.8)
+        
+        self.view.addSubview(titleLabel!)
+        self.view.bringSubviewToFront(titleLabel!)
+
+        
+    }
     
     func setTableViewConstrains() {
         self.tableView.translatesAutoresizingMaskIntoConstraints = false
